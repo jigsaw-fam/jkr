907a782e2596d694071b86c3f2cf3e0b557089df
diewland <diewland@gmail.com>
2024-07-14 23:23:37 +0700

    show tweet modal if tweet msg is not empty

--- www/app_v3.js
+++ www/app_v3.js
@@ -149,7 +149,7 @@ $('#mint').click(async _ => {
         $('#mint').removeClass('d-none');
         return;
       }
-      tweet_modal.show();
+      if (TWEET_TEXT) tweet_modal.show();
       play_party_effect();
       show_minted();
     })
