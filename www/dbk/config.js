897729d26d9b0058738e41784ddc972f16c3d7fe
diewland <diewland@gmail.com>
2024-07-14 21:49:01 +0700

    submit root folder

---
+++ www/dbk/config.js
@@ -0,0 +1,20 @@
+// chain
+const CHAIN_NAME      = "OP Mainnet";
+const CHAIN_RPC       = "https://mainnet.optimism.io";
+const CHAIN_ID        = 10;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://optimistic.etherscan.io";
+
+// contract
+const MAX_SUPPLY      = 0; // open edition
+const MINT_PER_WALLET = 5;
+const CONTRACT_ADDR   = "0x128a1e35398c72A9A060f482963f594752BECcEf";
+const MINT_GAS_RATE   = 1;
+
+// whitelist
+const PROOF_URL       = "https://bored-town.github.io/cdn/blobz/"; // null = public
+
+// twitter
+const TWEET_TEXT      = `Snagged my freemint Blobz NFT from @BoredTownNFT to celebrate the EIP-4844 'Dencun'! Gas fees were minimal thanks to the upgrade.
+
+Mint yours at https://boredtown.app/launch/blobz-op`;
