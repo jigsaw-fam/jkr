44554f370c5b1f280cd82791523f84f2152f847e
diewland <diewland@gmail.com>
2024-07-14 23:53:48 +0700

    update verify script (not work yet)

--- contracts/verify_contract.sh
+++ contracts/verify_contract.sh
@@ -1,4 +1,4 @@
-forge verify-contract --evm-version shanghai --constructor-args 0xf705Cb1501D04462aB7FF142F1F0ad198175725e --chain 20240603 --verifier blockscout --verifier-url https://scan.dbkchain.io/api --compiler-version 0.8.20 0xda4Ce631fBBbeF2E1aF0Ea2329d7E1c1fA78C7E2 contracts/JaiKaeRaeDBK_flattened.sol:JaiKaeRae
+forge verify-contract --evm-version shanghai --constructor-args 00000000000000000000000xf705cb1501d04462ab7ff142f1f0ad198175725e --chain 20240603 --verifier blockscout --verifier-url https://scan.dbkchain.io/api --compiler-version 0.8.20 0xda4Ce631fBBbeF2E1aF0Ea2329d7E1c1fA78C7E2 contracts/JaiKaeRaeDBK_flattened.sol:JaiKaeRae
 
 #Start verifying contract `0xda4Ce631fBBbeF2E1aF0Ea2329d7E1c1fA78C7E2` deployed on 20240603
 #
