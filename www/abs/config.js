fb857bca33de57f63fc7c6c1198f571f9b65b08d
diewland <diewland@gmail.com>
2024-08-15 13:13:38 +0700

    add abs mintpad

---
+++ www/abs/config.js
@@ -0,0 +1,21 @@
+// chain
+const CHAIN_NAME      = "Abstract Testnet";
+const CHAIN_RPC       = "https://api.testnet.abs.xyz";
+const CHAIN_ID        = 11124;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://explorer.testnet.abs.xyz";
+
+// contract
+const MAX_SUPPLY      = 10_000;
+const MINT_PER_WALLET = 10;
+const CONTRACT_ADDR   = "0x3ffB730429a9b78620c528B29246Ce2925346eA6";
+const MINT_GAS_RATE   = 1;
+const MINT_PRICE      = 0;
+const TOKEN_SYMBOL    = null;
+const TOKEN_ADDR      = null;
+
+// whitelist
+const PROOF_URL       = null;
+
+// twitter
+const TWEET_TEXT      = null;
