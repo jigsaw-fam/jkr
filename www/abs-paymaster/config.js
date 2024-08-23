ef29755add2ed4c7f26ba9f734a1957c270f95fd
diewland <diewland@gmail.com>
2024-08-23 17:12:30 +0700

    interact with paymaster contract (WIP)

---
+++ www/abs-paymaster/config.js
@@ -0,0 +1,22 @@
+// chain
+const CHAIN_NAME      = "Abstract Testnet";
+const CHAIN_RPC       = "https://api.testnet.abs.xyz";
+const CHAIN_ID        = 11124;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://explorer.testnet.abs.xyz";
+
+// contract
+const MAX_SUPPLY      = 10_000;
+const MINT_PER_WALLET = 10;
+const PAYMASTER_ADDR  = "0x4f6CFE5B81Ef9148fD08A58FB1109B106ae7DD5f";
+const CONTRACT_ADDR   = "0x3ffB730429a9b78620c528B29246Ce2925346eA6";
+const MINT_GAS_RATE   = 1;
+const MINT_PRICE      = 0;
+const TOKEN_SYMBOL    = null;
+const TOKEN_ADDR      = null;
+
+// whitelist
+const PROOF_URL       = null;
+
+// twitter
+const TWEET_TEXT      = null;
